798a1f56d01b86ac61e72ad28b6be9a61a815ece
f8tum <parthnaik.office@gmail.com>
2026-05-29 19:43:12 +0530

    feat: create content page

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,9 +1,16 @@
 import Sidebar from "./blocks/Sidebar";
+import Post from './blocks/Post';
+import Inspect from './blocks/Inspect';
 
 export default function Content() {
   return (
-    <main className="flex">
+    <main className="flex lg:gap-8">
       <Sidebar />
+      <section className='flex flex-col flex-1 lg:mt-10 lg:gap-6'>
+        <Post />
+        <Post />
+      </section>
+      <Inspect />
     </main>
   )
 }
