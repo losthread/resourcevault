dbcbf2f39015ae6b04b20ecf43378ebcfca88c76
f8tum <parthnaik.office@gmail.com>
2026-05-30 00:23:22 +0530

    refactor: add contributions placeholder

--- frontend/src/components/blocks/Post.jsx
+++ frontend/src/components/blocks/Post.jsx
@@ -1,6 +1,6 @@
 import { Button } from '../ui/button';
 import Tag from './Tag';
-import { ArrowBigUp, ArrowBigDown, Bookmark, Share2, NotepadText, Flag } from 'lucide-react';
+import { ArrowBigUp, ArrowBigDown, Bookmark, Share2, NotepadText, Flag, GitPullRequest } from 'lucide-react';
 import {
   Card,
   CardContent,
@@ -34,33 +34,42 @@ export default function post() {
 
       <CardFooter>
         {/* author details */}
-        <div className='flex lg:gap-3 items-center'>
+        <div className='flex lg:gap-3 w-full items-center justify-between'>
           <div className='flex lg:gap-2 items-center'>
-            <div className='flex items-center bg-accent rounded-4xl'>
-              <Button size='icon-lg' variant='ghost' className='hover:text-red-500' >
-                <ArrowBigUp size={30} />
+            <div className='flex lg:gap-2'>
+              <div className='flex items-center bg-accent rounded-4xl'>
+                <Button size='icon-lg' variant='ghost' className='hover:text-red-500' >
+                  <ArrowBigUp size={30} />
+                </Button>
+                <p className='lg:text-sm'>69</p>
+              <Button size='icon-lg' variant='ghost' className='hover:text-purple-600' >
+                  <ArrowBigDown size={20} />
+                </Button>
+              </div>
+
+              <Button size='lg' variant='ghost' className='bg-accent rounded-4xl'>
+                <Share2 size={20} />
+                Share
               </Button>
-              <p className='lg:text-sm'>69</p>
-             <Button size='icon-lg' variant='ghost' className='hover:text-purple-600' >
-                <ArrowBigDown size={20} />
+
+              <Button size='icon-lg' variant='ghost' className='bg-accent rounded-4xl'>
+                <Bookmark size={20} />
+              </Button>
+
+              <Button  size='icon-lg' variant='ghost' className='bg-accent rounded-4xl'>
+                <NotepadText size={20} />
+              </Button>
+
+              <Button  size='icon-lg' variant='destructive' className='bg-accent rounded-4xl'>
+                <Flag size={20} />
               </Button>
             </div>
+          </div>
 
-            <Button size='lg' variant='ghost' className='bg-accent rounded-4xl'>
-              <Share2 size={20} />
-              Share
-            </Button>
-
-            <Button size='icon-lg' variant='ghost' className='bg-accent rounded-4xl'>
-              <Bookmark size={20} />
-            </Button>
-
-            <Button  size='icon-lg' variant='ghost' className='bg-accent rounded-4xl'>
-              <NotepadText size={20} />
-            </Button>
-
-            <Button  size='icon-lg' variant='ghost' className='bg-accent rounded-4xl'>
-              <Flag size={20} />
+          <div>
+            <Button  size='lg' variant='secondary' className='bg-accent rounded-2xl'>
+              <GitPullRequest size={20} />
+              Add Resource
             </Button>
           </div>
         </div>
